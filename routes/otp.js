72670a7299a77d59442c1562efc7ee34c37c151c
Sinojiya Manthan <sinojiyamonu@gmail.com>
2025-12-04 13:56:02 +0530

    Update for otp verification

--- routes/otp.js
+++ routes/otp.js
@@ -1,3 +1,18 @@
+// // export default router;
+
+// import express from "express";
+// import { registerUser, verifyOtpAndCreate } from "../controllers/otpController.js";
+// import {
+//   otpSendValidation,
+//   otpVerifyValidation
+// } from "../validators/allValidations.js";
+// import { validate } from "../middleware/validationHandler.js";
+
+// const router = express.Router();
+
+// router.post("/register", otpSendValidation, validate, registerUser);
+// router.post("/verify-otp", otpSendValidation, validate, verifyOtpAndCreate);
+
 // export default router;
 
 import express from "express";
@@ -11,6 +26,6 @@ import { validate } from "../middleware/validationHandler.js";
 const router = express.Router();
 
 router.post("/register", otpSendValidation, validate, registerUser);
-router.post("/verify-otp", otpSendValidation, validate, verifyOtpAndCreate);
+router.post("/verify-otp", otpVerifyValidation, validate, verifyOtpAndCreate);
 
 export default router;
--- services/msg91Service.js
+++ services/msg91Service.js
@@ -1,3 +1,34 @@
+// // services/msg91Service.js
+// import fetch from "node-fetch";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const sendOtp = async (mobile, otp) => {
+//   const url = "https://api.msg91.com/api/v5/otp";
+
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: {
+//       accept: "application/json",
+//       authkey: process.env.MSG91_AUTH_KEY,
+//       "content-type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       mobile: `91${mobile}`,
+//       otp,
+//       template_id: process.env.MSG91_OTP_TEMPLATE_ID,
+//     }),
+//   });
+
+//   const data = await response.json();
+//   console.log("MSG91 Response:", data);
+
+//   if (!response.ok) throw new Error(data.message || "Failed to send OTP");
+
+//   return true;
+// };
+
+
 // services/msg91Service.js
 import fetch from "node-fetch";
 import dotenv from "dotenv";
@@ -21,9 +52,11 @@ export const sendOtp = async (mobile, otp) => {
   });
 
   const data = await response.json();
-  console.log("MSG91 Response:", data);
+  console.log("MSG91 Send OTP:", data);
 
-  if (!response.ok) throw new Error(data.message || "Failed to send OTP");
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to send OTP");
+  }
 
   return true;
 };
