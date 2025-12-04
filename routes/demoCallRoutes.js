f939a52b19ab2f068b3c918259dc16b12d535590
Sinojiya Manthan <sinojiyamonu@gmail.com>
2025-12-04 16:16:50 +0530

    Update ScheduleCallModal for otp verification.

--- routes/demoCallRoutes.js
+++ routes/demoCallRoutes.js
@@ -1,38 +1,38 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import DemoCall from "../models/DemoCall.js";
-import { sendOtpMsg91, verifyOtpMsg91 } from "../services/msg91Service.js";
+// import { sendOtpMsg91, verifyOtpMsg91 } from "../services/msg91Service.js";
 
 const router = express.Router();
 
 // SEND OTP
-router.post(
-  "/send-otp",
-  asyncHandler(async (req, res) => {
-    await sendOtpMsg91(req.body.mobile);
+// router.post(
+//   "/send-otp",
+//   asyncHandler(async (req, res) => {
+//     await sendOtpMsg91(req.body.mobile);
 
-    res.status(200).json({
-      message: "OTP sent successfully",
-    });
-  })
-);
+//     res.status(200).json({
+//       message: "OTP sent successfully",
+//     });
+//   })
+// );
 
 // VERIFY OTP
-router.post(
-  "/verify-otp",
-  asyncHandler(async (req, res) => {
-    const result = await verifyOtpMsg91(req.body.mobile, req.body.otp);
+// router.post(
+//   "/verify-otp",
+//   asyncHandler(async (req, res) => {
+//     const result = await verifyOtpMsg91(req.body.mobile, req.body.otp);
 
-    if (!result || result.type !== "success") {
-      res.status(400);
-      throw new Error("Invalid OTP");
-    }
+//     if (!result || result.type !== "success") {
+//       res.status(400);
+//       throw new Error("Invalid OTP");
+//     }
 
-    res.status(200).json({
-      message: "OTP verified successfully",
-    });
-  })
-);
+//     res.status(200).json({
+//       message: "OTP verified successfully",
+//     });
+//   })
+// );
 
 // BOOK DEMO
 router.post(
