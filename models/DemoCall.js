dad543cda56e91af5b8f7c0fd40a57d134ae4e3b
Sinojiya Manthan <sinojiyamonu@gmail.com>
2025-12-07 11:22:28 +0530

    DemoCall routes Cretae Delete Api

--- models/DemoCall.js
+++ models/DemoCall.js
@@ -4,8 +4,15 @@ const demoCallSchema = new mongoose.Schema(
   {
     fullName: { type: String, required: true },
     email: { type: String, required: true },
-    mobile: { type: String, required: false },
+    mobile: { type: String },
     message: { type: String },
+
+    // ADD THIS FIELD
+    status: {
+      type: String,
+      enum: ["not_contacted", "called"],
+      default: "not_contacted",
+    },
   },
   { timestamps: true }
 );
