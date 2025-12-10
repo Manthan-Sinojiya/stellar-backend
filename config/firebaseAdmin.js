55dbf3aee3db34e73e849333db70e9bb728da070
Sinojiya Manthan <sinojiyamonu@gmail.com>
2025-12-10 12:05:20 +0530

    Google update

---
+++ config/firebaseAdmin.js
@@ -0,0 +1,10 @@
+// config/firebaseAdmin.js
+import admin from "firebase-admin";
+
+import serviceAccount from "./firebase-service-account.json" assert { type: "json" };
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
