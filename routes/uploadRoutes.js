cc6990c6efe74cbbc76bc63610c9f897d7817161
Sinojiya Manthan <sinojiyamonu@gmail.com>
2025-12-26 16:57:17 +0530

    Solving PDF Updation .

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { getSignedUploadUrl } from "../utils/s3.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/presigned-url", protect, async (req, res) => {
+  const { fileName, fileType, folder } = req.body;
+
+  if (!fileName || !fileType) {
+    return res.status(400).json({ message: "Invalid file data" });
+  }
+
+  const data = await getSignedUploadUrl({
+    fileName,
+    fileType,
+    folder,
+  });
+
+  res.json(data);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/demo-call", demoCallRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/settings", settingsRoutes);
-app.use("/uploads", express.static("uploads"));
+app.use("/api/upload", uploadRoutes);
 
 /* ------------------------------------------------------------------
    GLOBAL ERROR HANDLER
