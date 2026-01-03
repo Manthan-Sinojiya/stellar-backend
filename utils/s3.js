d143e15613505a4a89e49a194487a3be51c07fcb
Sinojiya Manthan <sinojiyamonu@gmail.com>
2026-01-03 17:44:01 +0530

    Add PDF for Download Brochure

--- utils/s3.js
+++ utils/s3.js
@@ -1,30 +1,13 @@
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-// const s3 = new S3Client({
-//   region: process.env.AWS_REGION,
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   },
-// });
-
-// // export const getSignedUploadUrl = async ({ fileName, fileType, folder }) => {
-// //   const key = `${folder}/${Date.now()}-${fileName}`;
-
-// //   const command = new PutObjectCommand({
-// //     Bucket: process.env.AWS_BUCKET,
-// //     Key: key,
-// //     ContentType: fileType,
-// //   });
-
-// //   const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 300 });
-
-// //   return {
-// //     uploadUrl,
-// //     fileUrl: `https://${process.env.AWS_BUCKET}.s3.amazonaws.com/${key}`,
-// //   };
-// // };
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 // export const getSignedUploadUrl = async ({ fileName, fileType, folder }) => {
 //   const key = `${folder}/${Date.now()}-${fileName}`;
@@ -39,37 +22,24 @@
 
 //   return {
 //     uploadUrl,
-//     // CHANGE THIS: Return only the key/path to store in the DB
-//     fileUrl: `/${key}`, 
+//     fileUrl: `https://${process.env.AWS_BUCKET}.s3.amazonaws.com/${key}`,
 //   };
 // };
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
-const s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-
 export const getSignedUploadUrl = async ({ fileName, fileType, folder }) => {
-  const timestamp = Date.now();
-  const key = `${folder}/${timestamp}-${fileName}`;
+  const key = `${folder}/${Date.now()}-${fileName}`;
 
   const command = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET,
     Key: key,
     ContentType: fileType,
-    // Optional: ACL: 'public-read' // Only if bucket allows ACLs
   });
 
   const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 300 });
 
   return {
     uploadUrl,
-    fileUrl: `/${key}`, // Ensure this starts with a slash
+    // CHANGE THIS: Return only the key/path to store in the DB
+    fileUrl: `/${key}`, 
   };
 };
