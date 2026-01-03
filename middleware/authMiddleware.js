a5c9ee33519684bbd859ee7b351efc68e1dfefe5
Sinojiya Manthan <sinojiyamonu@gmail.com>
2026-01-03 11:15:42 +0530

    Update a QuizResultcontroller

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -39,9 +39,6 @@ import asyncHandler from "express-async-handler";
 //   }
 // });
 
-import jwt from "jsonwebtoken";
-import asyncHandler from "express-async-handler";
-import User from "../models/userModel.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
   const authHeader =
