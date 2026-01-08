79b0ca002f0c69e24ce9c0a534a2860fe92ec466
Sinojiya Manthan <sinojiyamonu@gmail.com>
2026-01-08 14:06:59 +0530

    Update a Sendpaymentemail pdfc

--- utils/sendPaymentEmail.js
+++ utils/sendPaymentEmail.js
@@ -776,7 +776,7 @@ export const sendPaymentSuccessEmail = async (user) => {
 
     // 3. Title & Metadata
     doc
-      .fillColor("#ffffff")
+      .fillColor("#cf27e9ff")
       .font("Helvetica-Bold")
       .fontSize(20)
       .text("PAYMENT RECEIPT", 115, 35, { characterSpacing: 1 });
