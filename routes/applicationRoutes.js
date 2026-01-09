764e4832d15631ad0a884a90bcb96ca0ec81c844
Sinojiya Manthan <sinojiyamonu@gmail.com>
2026-01-09 20:30:44 +0530

    Integrate a Payment Method

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -39,7 +39,7 @@ router.get("/education", protect, getEducation);
 // STEP 3 – payment
 router.post("/payment/create-order", protect, createOrder);
 router.post("/payment/verify", protect, verifyPayment);
-router.route("/all").get(getAllPayments);
+router.route("/all").get(protect, adminOnly, getAllPayments);
 
 // STEP 4 – Aptitude (NEW)
 router.post("/aptitude/complete", protect, completeAptitudeStep);
